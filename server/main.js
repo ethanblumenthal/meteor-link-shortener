0ffa689c5b1cf0fe0560a9e8d65a2f698ea75963
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-22 11:21:10 -0500

    updated mongo records to reflect click count

--- server/main.js
+++ server/main.js
@@ -13,6 +13,7 @@ const onRoute = (req, res, next) => {
   const link = Links.findOne({ token: req.params.token })
 
   if (link) {
+    Links.update(link, { $inc: { clicks: 1 } })
     res.writeHead(307, { 'Location': link.url })
     res.end()
   } else {
