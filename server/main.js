3264d1f8a97e7a7489addb48ec4e8fd390709a22
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-22 11:17:22 -0500

    created middleware to implement short link redirects

--- server/main.js
+++ server/main.js
@@ -1,8 +1,27 @@
+import ConnectRoute from 'connect-route'
 import { Meteor } from 'meteor/meteor'
 import { Links } from '../imports/collections/Links'
+import { WebApp } from 'meteor/webapp'
 
 Meteor.startup(() => {
   Meteor.publish('links', function() {
     return Links.find({})
   })
 })
+
+const onRoute = (req, res, next) => {
+  const link = Links.findOne({ token: req.params.token })
+
+  if (link) {
+    res.writeHead(307, { 'Location': link.url })
+    res.end()
+  } else {
+    next()
+  }
+}
+
+const middleware = ConnectRoute(router => {
+  router.get('/:token', onRoute)
+})
+
+WebApp.connectHandlers.use(middleware)
