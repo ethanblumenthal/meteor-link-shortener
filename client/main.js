3a334f0c5122dab46c7a73d5437462b97b59ec5b
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-22 10:05:30 -0500

    created header component

--- client/main.js
+++ client/main.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <div>
+      <Header />
+    </div>
   )
 }
 
