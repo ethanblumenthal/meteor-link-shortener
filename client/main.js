c09a4ffcfb656597b2f3cf6d7db60d0d57023d3d
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-22 11:04:25 -0500

    created link list component to display table of shortened links

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/Header'
 import LinkCreate from './components/LinkCreate'
+import LinkList from './components/LinkList'
 import { Links } from '../imports/collections/Links'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <div>
       <Header />
       <LinkCreate />
+      <LinkList />
     </div>
   )
 }
